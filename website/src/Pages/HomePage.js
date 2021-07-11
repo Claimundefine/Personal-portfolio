3490d70079fadd25d8c6f4448a4914d8668f7dac
Claimundefine <jjustin.wangg@gmail.com>
2021-07-11 00:13:27 -0400

    made build and added some fluff to home page

--- website/src/Pages/HomePage.js
+++ website/src/Pages/HomePage.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am Justin
+                    <span>
+                        Wang
+                    </span>
+                </h1>
+                {/* <p>
+                    Jungle Kingdom
+                    Jungle Kingdom
+                    ADC Kingdom
+                </p> */}
+                <div className="icons">
+
+                </div>
+            </header>
         </div>
     )
 }
